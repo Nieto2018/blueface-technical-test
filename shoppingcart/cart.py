473b481b0b5c6a778e7d76516a644ba191a2a861
Antonio Nieto <nietodjsombra@gmail.com>
2020-02-18 18:47:10 +0000

    Small fixes

--- shoppingcart/cart.py
+++ shoppingcart/cart.py
@@ -6,7 +6,7 @@ from . import abc
 CURRENCIES = {
     "eur": [1, "€%.2f"],
     "usd": [1.1, "$%.2f"],
-    "gbp": [1.27, "$%.2f"]
+    "gbp": [1.27, "£%.2f"]
 }
 
 
@@ -18,56 +18,33 @@ class ShoppingCart(abc.ShoppingCart):
         self._items = dict()
 
         self._products = self._get_products()
-        # self._currencies = {
-        #     "eur": [1, "€%.2f"],
-        #     "usd": [1.1, "$%.2f"],
-        #     "gbp": [1.27, "$%.2f"]
-        # }
         self._set_currency(currency_code)
 
     def _set_currency(self, currency_code):
-        # if currency_code in self._currencies.keys():
         if currency_code in CURRENCIES.keys():
             self._currency = currency_code
         else:
+            # Default currency EUR
             self._currency = 'eur'
 
     def _get_products(self):
         products = {}
 
         try:
-            # with open("../products.json") as file:
-            with open("/home/nieto/Escritorio/test-shoppingcart/products.json") as file:
+            with open("../products.json") as file:
                 data = json.load(file)
                 for product in data['products']:
                     name = product['name']
                     price = product['price']
                     products[name] = price
         except FileNotFoundError:
-            print("File not found")
+            print("The product file not found")
         except:
             print("Error opening the file")
 
         return products
 
-    def add_item(self, product_code: str, quantity: int):
-        if product_code not in self._items:
-            self._items[product_code] = quantity
-        else:
-            q = self._items[product_code]
-            self._items[product_code] = q + quantity
-
     def print_receipt(self) -> typing.List[str]:
-        # lines = []
-        #
-        # for item in self._items.items():
-        #     price = self._get_product_price(item[0]) * item[1]
-        #
-        #     price_string = "€%.2f" % price
-        #
-        #     lines.append(item[0] + " - " + str(item[1]) + ' - ' + price_string)
-        #
-        # return lines
         lines = []
         total_price = 0
 
@@ -75,28 +52,27 @@ class ShoppingCart(abc.ShoppingCart):
             price = self._get_product_price(item[0]) * item[1]
             total_price = total_price + price
 
-            price_string = self._get_price_by_currency(price, self._currency)
+            price_string = self._get_price_by_currency(price)
 
             lines.append(item[0] + " - " + str(item[1]) + ' - ' + price_string)
 
-        total_price_string = self._get_price_by_currency(total_price, self._currency)
+        total_price_string = self._get_price_by_currency(total_price)
         lines.append('Total - ' + total_price_string)
 
         return lines
 
+    def add_item(self, product_code: str, quantity: int):
+        # To avoid that an item appears in the item list if it is not in the product list
+        if product_code not in self._products.keys():
+            raise Exception("The selected product is not in the product list")
+
+        if product_code not in self._items:
+            self._items[product_code] = quantity
+        else:
+            q = self._items[product_code]
+            self._items[product_code] = q + quantity
+
     def _get_product_price(self, product_code: str) -> float:
-        # price = 0.0
-        #
-        # if product_code == 'apple':
-        #     price = 1.0
-        #
-        # elif product_code == 'banana':
-        #     price = 1.1
-        #
-        # elif product_code == 'kiwi':
-        #     price = 3.0
-        #
-        # return price
         price = 0.0
 
         if product_code in self._products.keys():
@@ -104,19 +80,8 @@ class ShoppingCart(abc.ShoppingCart):
 
         return price
 
-    def _get_price_by_currency(self, price: float, currency_code: str) -> str:
-
-        # if currency_code in self._currencies.keys():
-        # if currency_code in self._currencies.keys():
-        #     currency = self._currencies[currency_code]
-        # else:
-        #     # Default currency EUR
-        #     currency = self._currencies["eur"]
-        if currency_code in CURRENCIES.keys():
-            currency = CURRENCIES[currency_code]
-        else:
-            # Default currency EUR
-            currency = CURRENCIES["eur"]
+    def _get_price_by_currency(self, price: float) -> str:
+        currency = CURRENCIES[self._currency]
 
         currency_price = price * currency[0]
         currency_price_string = currency[1] % currency_price
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -12,6 +12,7 @@ class TestCartMethods(unittest.TestCase):
         receipt = cart.print_receipt()
 
         assert receipt[0] == "apple - 1 - €1.00"
+        assert receipt[1] == "Total - €1.00"
 
     def test_add_item_with_multiple_quantity(self):
         cart = ShoppingCart()
@@ -20,6 +21,7 @@ class TestCartMethods(unittest.TestCase):
         receipt = cart.print_receipt()
 
         assert receipt[0] == "apple - 2 - €2.00"
+        assert receipt[1] == "Total - €2.00"
 
     def test_add_different_items(self):
         cart = ShoppingCart()
@@ -30,8 +32,19 @@ class TestCartMethods(unittest.TestCase):
 
         assert receipt[0] == "banana - 1 - €1.10"
         assert receipt[1] == "kiwi - 1 - €3.00"
+        assert receipt[2] == "Total - €4.10"
 
-    def test_total(self):
+    def test_total_with_wrong_currency(self):
+        # Default currency is EUR
+        cart = ShoppingCart('xxx')
+        cart.add_item("banana", 2)
+
+        receipt = cart.print_receipt()
+
+        assert receipt[0] == "banana - 2 - €2.20"
+        assert receipt[1] == "Total - €2.20"
+
+    def test_total_in_eur(self):
         cart = ShoppingCart()
         cart.add_item("banana", 2)
         cart.add_item("kiwi", 3)
@@ -41,3 +54,25 @@ class TestCartMethods(unittest.TestCase):
         assert receipt[0] == "banana - 2 - €2.20"
         assert receipt[1] == "kiwi - 3 - €9.00"
         assert receipt[2] == "Total - €11.20"
+
+    def test_total_in_usd(self):
+        cart = ShoppingCart('usd')
+        cart.add_item("banana", 2)
+        cart.add_item("kiwi", 3)
+
+        receipt = cart.print_receipt()
+
+        assert receipt[0] == "banana - 2 - $2.42"
+        assert receipt[1] == "kiwi - 3 - $9.90"
+        assert receipt[2] == "Total - $12.32"
+
+    def test_total_in_gbp(self):
+        cart = ShoppingCart('gbp')
+        cart.add_item("banana", 2)
+        cart.add_item("kiwi", 3)
+
+        receipt = cart.print_receipt()
+
+        assert receipt[0] == "banana - 2 - £2.79"
+        assert receipt[1] == "kiwi - 3 - £11.43"
+        assert receipt[2] == "Total - £14.22"
